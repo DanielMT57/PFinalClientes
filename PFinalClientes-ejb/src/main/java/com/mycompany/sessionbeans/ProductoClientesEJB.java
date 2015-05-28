50bef45bf614c1600a5718a1656cc22539652c63
german <german@10.1.3.30>
2015-05-28 00:07:27 -0500

    Cambios de ejbs para desplegar

---
+++ PFinalClientes-ejb/src/main/java/com/mycompany/sessionbeans/ProductoClientesEJB.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sessionbeans;
+
+import com.mycompany.Productos;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Daniel
+ */
+@LocalBean
+@Stateless
+public class ProductoClientesEJB extends EJBGenericoCliente<Productos>{
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+}
