0f31c1afd5e2a34a316b99d61af8cf7dda54c663
german <german@german-PC>
2015-05-30 17:41:20 -0500

    Cambios para cargar lista de la Base de Datos

--- PFinalClientes-web/src/main/java/com/mycompany/mbeans/CatalogoMBean.java
+++ PFinalClientes-web/src/main/java/com/mycompany/mbeans/CatalogoMBean.java
@@ -5,10 +5,13 @@
  */
 package com.mycompany.mbeans;
 
+import com.mycompany.Catalogo;
 import com.mycompany.sessionbeans.CatalogoClienteEJB;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
@@ -21,18 +24,81 @@ import javax.faces.view.ViewScoped;
 @ManagedBean
 @Named(value = "catalogoMBean")
 @ViewScoped
-public class CatalogoMBean implements Serializable{
+public class CatalogoMBean implements Serializable {
+
     private int id;
     private Date fechaInicio;
     private Date fechaFin;
     private int cantidadPaginas;
     private int cantidadProductos;
-
+    
+    
     @EJB
-    private CatalogoClienteEJB catalogoejb;
-  
+    private CatalogoClienteEJB catalogClienteEJB;
 
-    /**
-     * Creates a new instance of CatalogoMBean
-     */ 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public int getCantidadPaginas() {
+        return cantidadPaginas;
+    }
+
+    public void setCantidadPaginas(int cantidadPaginas) {
+        this.cantidadPaginas = cantidadPaginas;
+    }
+
+    public int getCantidadProductos() {
+        return cantidadProductos;
+    }
+
+    public void setCantidadProductos(int cantidadProductos) {
+        this.cantidadProductos = cantidadProductos;
+    }
+
+    public CatalogoClienteEJB getCatalogoejb() {
+        return catalogClienteEJB;
+    }
+
+    public void setCatalogoejb(CatalogoClienteEJB catalogoejb) {
+        this.catalogClienteEJB = catalogoejb;
+    }
+
+    private List<Catalogo> catalogos;
+
+  
+    public List<Catalogo> getCatalogos() {
+        catalogos=catalogClienteEJB.listarTodos();
+        return catalogos;
+    }
+
+   
+
+    private void limpiar() {
+        setId(0);
+        setFechaInicio(null);
+        setFechaFin(null);
+        setCantidadPaginas(0);
+        setCantidadProductos(0);
+    }
 }
