23fd297d28d6a3241cc0b548ed007e6acb911381
MAO <MAO@MAO-PC>
2015-06-01 13:07:48 -0500

    Creacion de ejb de venta y detalle queda pendiente crear en managedbean de detalleventa y buscar  errores de insertarpedidos

---
+++ PFinalClientes-ejb/src/main/java/com/mycompany/sessionbeans/DetalleVentaEJB.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.sessionbeans;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import com.mycompany.DetalleVenta;
+
+/**
+ *
+ * @author MAO
+ */
+@Stateless
+@LocalBean
+public class DetalleVentaEJB extends EJBGenericoCliente<DetalleVenta>{
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ PFinalClientes-ejb/src/main/java/com/mycompany/sessionbeans/VentasClientesEJB.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.sessionbeans;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import com.mycompany.VentasClientes;
+
+/**
+ *
+ * @author MAO
+ */
+@Stateless
+@LocalBean
+public class VentasClientesEJB extends EJBGenericoCliente<VentasClientes>{
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
